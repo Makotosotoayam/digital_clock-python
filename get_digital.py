d2d1a21259edf120a8df8e6f0162a00419d6d070
= <=>
2024-12-01 17:22:39 +0700

    We off the grid

---
+++ get_digital.py
@@ -0,0 +1,55 @@
+import sys
+from PyQt5.QtWidgets import QApplication, QWidget, QLabel, QVBoxLayout
+from PyQt5.QtCore import QTimer, QTime, Qt
+from PyQt5.QtGui import QFont, QFontDatabase
+import os
+
+class DigitalClock(QWidget):
+    def __init__(self):
+        super().__init__()
+        self.time_label = QLabel(self)
+        self.timer = QTimer(self)
+        self.initUI()
+
+    def initUI(self):
+        self.setWindowTitle("Digital Clock")
+        self.setGeometry(600, 400, 300, 100)
+
+        
+        vbox = QVBoxLayout()
+        vbox.addWidget(self.time_label)
+        self.setLayout(vbox)
+
+        
+        self.time_label.setAlignment(Qt.AlignCenter)
+        self.time_label.setStyleSheet("font-size: 48px; color: black;")
+        self.setStyleSheet("background-color: #1DB954;")
+
+    
+        
+        base_path = os.path.expanduser("~/Downloads/ds_digital")
+        font_file = "DS-DIGIT.ttf"
+        font_path = os.path.join(base_path,font_file)
+        font_id = QFontDatabase.addApplicationFont(font_path)
+        
+        if font_id != -1:
+            font_family = QFontDatabase.applicationFontFamilies(font_id)[0]
+            my_font = QFont(font_family, 48)
+            self.time_label.setFont(my_font)
+
+
+        self.timer.timeout.connect(self.update_time)
+        self.timer.start(1000)
+
+
+        self.update_time()
+
+    def update_time(self):
+        current_time = QTime.currentTime().toString("hh:mm:ss AP")
+        self.time_label.setText(current_time)
+
+if __name__ == "__main__":
+    app = QApplication(sys.argv)
+    clock = DigitalClock()
+    clock.show()
+    sys.exit(app.exec_())
